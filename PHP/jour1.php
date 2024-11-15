b5dc8450fcec34e48807130c5cdc4c7cc4e4ad49
Johnny <johnny.sass2001@gmail.com>
2024-11-15 11:18:22 +0100

    matin

---
+++ PHP/jour1.php
@@ -0,0 +1,32 @@
+<?php
+
+$months = [
+    "janvier" => ["jours" => 31, "saison" => "hiver"],
+    "fevrier" => ["jours" => 28, "saison" => "hiver"],
+    "mars" => ["jours" => 31, "saison" => "printemps"],
+    "avril" => ["jours" => 30, "saison" => "printemps"],
+    "mai" => ["jours" => 31, "saison" => "printemps"],
+    "juin" => ["jours" => 30, "saison" => "printemps"],
+    "juillet" => ["jours" => 31, "saison" => "été"],
+    "aout" => ["jours" => 31, "saison" => "été"],
+    "septembre" => ["jours" => 30, "saison" => "automne"],
+    "octobre" => ["jours" => 31, "saison" => "automne"],
+    "novembre" => ["jours" => 30, "saison" => "automne"],
+    "décembre" => ["jours" => 31, "saison" => "hiver"]
+];
+
+
+    // var_dump($months);
+    // echo  $months[0];
+
+echo " nombre de jour : {$months['septembre']['jours']} saison: {$months['septembre']['saison']}";
+
+foreach ($months as $key => $value) {
+echo "mois:  $key nbres de jours:  {$value["jours"]} saison{$value['saison']} <br>";}
+
+for ($i = 1; $i < 11; $i++) {
+    $res = $i * 2;
+    echo "$i X 2 = {$res} <br>";
+}
+?>
+
---
+++ PHP/jour2.php
@@ -0,0 +1,69 @@
+<?php
+echo "- if - <br>";
+
+$note = 0;
+
+if ($note >= 0 && $note < 10) {
+    echo "insuffisant";
+} elseif ($note >= 10 && $note < 12) {
+    echo "bien";
+} elseif ($note >= 12) {
+    echo "très bien";
+} else {
+    echo "Note invalide";
+}
+
+echo "<br> - switch - <br>";
+
+$code = "403";
+
+
+switch ($code) {
+    case '200':
+        echo 'succès';
+        break;
+    case '400':
+        echo 'bad request';
+        break;
+    case '500':
+        echo 'erreur interne';
+        break;
+    case '403':
+        echo 'forbidden';
+        break;
+    default:
+        echo 'erreur inconnue';
+        break;
+}
+
+echo "<br> - switch avec tableau - <br>";
+
+
+$errorCodes = [200, 400, 500, 403];
+shuffle($errorCodes);
+
+
+
+switch ($errorCodes[0]) {
+    case 200:
+        echo "succès";
+        break;
+    case 400:
+        echo "bad request";
+        break;
+    case 500:
+        echo "erreur interne";
+        break;
+    case 403:
+        echo "forbidden";
+        break;
+    default:
+        echo "erreur inconnue";
+        break;
+}
+
+echo "<br> - Les fonction - <br>";
+
+
+
+?>
