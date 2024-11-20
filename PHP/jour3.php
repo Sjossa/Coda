d433113e918dd2eeba5e3d15208a1bef1a26b926
Johnny <johnny.sass2001@gmail.com>
2024-11-20 14:50:44 +0100

    ffffff dfgdg

--- PHP/jour3.php
+++ PHP/jour3.php
@@ -1,3 +1,23 @@
+<?php
+
+if ( isset( $_POST['valid'] ) ) {
+    // if ( isset( $_POST['username'] ) ) {
+    //     $username = $_POST['username'];
+    // }
+
+    $username = !empty($_POST['username']) ? $_POST['username'] : null;
+    $pass = !empty($_POST['pass']) ? $_POST['pass'] : null;
+   
+    if($username !== null && 
+    $pass !== null && 
+    filter_var($username, FILTER_VALIDATE_EMAIL)) 
+    {
+
+
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,8 +25,9 @@
     <title>Mon formulaire</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body cl>
-    <div class="container">
+<body>
+    <!-- entrainement -->
+    <!-- <div class="container">
         <form method="post">
             <div class="mb-3">
                 <label for="lastname" class="form-label">Nom</label>
@@ -39,6 +60,24 @@
 
             <button type="submit" class="btn btn-primary">Valider</button>
         </form>
+    </div> -->
+
+ <!--test   -->
+    <div class="container">
+    <form method="post">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">identifiant</label>
+    <input type="email" class="form-control" name="username" id="username" required>
+  </div>
+
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">mot de passe</label>
+    <input type="password" class="form-control" id="pass" name="pass" required>
+  </div>
+
+  <button type="submit" class="btn btn-primary" name="valid">valider</button>
+
+</form>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
