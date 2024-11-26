96786c477be23be32442d4d78f5ea39364d5f1ba
Johnny <johnny.sass2001@gmail.com>
2024-11-26 13:43:53 +0100

    MARDI

--- PHP/jour4.php
+++ PHP/jour4.php
@@ -1,136 +1,229 @@
 <?php
 require "jour3_functions.php";
-$errors = [];
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=coda_school', 'root');
-} catch (Exception $e) {
-    $errors[] = "Erreur de connexion à la bdd {$e->getMessage()}";
-}
-if (isset($_GET["id"])) {
-    $id = $_GET['id'];
-    if (!is_numeric($id)) {
-
-        $errors[] = "id au mauvais format";
-
-    } else {
-
+    $error = [];
+    try {
+        $pdo = new PDO("mysql:host=localhost;dbname=coda_school", "root");
+    } catch (Exception $e) {
+        $error[] = "Erreur de connexion a la BDD{$e->getMessage()}";
     }
-}
-if (isset($_POST['valid_button'])) {
-    $errors = [];
-    $username = !empty($_POST['username']) ? $_POST['username'] : null;
-    $password = !empty($_POST['pass']) ? $_POST['pass'] : null;
-    $confirmation = !empty($_POST['confirmation']) ? $_POST['confirmation'] : null;
-    $email = !empty($_POST['email']) ? $_POST['email'] : null;
-    $enabled = !empty($_POST['enabled']) ? true : false;
 
-    if (
-        !empty($username) &&
-        !empty($email) &&
+    if(isset($_POST["modif_button"]) && empty($error)) {
+        $username = !empty($_POST['name'])? $_POST['name'] : null;
+        $email = !empty($_POST['mail'])? $_POST['mail'] : null;
+        $password = !empty($_POST['pass'])? $_POST['pass'] : null;
+        $cpass = !empty($_POST['cpassword'])? $_POST['cpassword'] : null;
+        $enable = !empty($_POST['enable'])? true : false;
+
+        $id = $_GET["id"];
+        if(!is_numeric($id)){
+            $error[] = "ID au mauvais format";
+        }
+
+        if(
+            $username != null &&
+            $email != null
+            ) {
+
+            $username = cleanString($username);
+            $email = cleanString($email);
+
+
+
+
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $error[] = "L'email n'est pas valide";
+            }
+
+
+            try {
+                $state = $pdo->prepare("SELECT COUNT(*) AS usernb FROM `users` WHERE `email` = :mail");
+                $state->bindParam(':mail', $email);
+                $state->execute();
+                $result = $state->fetch();
+            } catch (Exception $e) {
+                $error[] = " Erreur de verifiaction de la BDD{$e->getMessage()}";
+            }
+
+            if($result['usernb'] !== 0){
+                $error[] = "l'email est déjà utiliser";
+            }
+
+            if(empty($error)) {
+                try {
+                    $statement = $pdo->prepare("UPDATE `users` SET username = :username, email = :email, enabled = :enabled WHERE id = :id");
+                    $statement->bindParam(":username", $username);
+                    $statement->bindParam(":email", $email);
+                    $statement->bindParam(":enabled", $enable, PDO::PARAM_BOOL);
+                    $statement->bindParam(":id", $id, PDO::PARAM_INT);
+                    $statement->execute();
+                } catch (Exception $e) {
+                    $error[] = "Erreur de requête : {$e->getMessage()}";
+                }
+            }
+
+        }
+    }
+    if(
         !empty($password) &&
-        !empty($confirmation)
-    ) {
-        $username = cleanString($username);
-        $email = cleanString($email);
-        $password = cleanString($password);
-        $confirmation = cleanString($confirmation);
+        !empty($cpass)
+    ){
+        $password = cleanCodeString($password);
+            $cpass = cleanCodeString($cpass);
 
-        if ($confirmation !== $password) {
-            $errors[] = 'Le mot de passe et sa confirmation sont différents';
-        } else {
-            $confirmation = null;
-            $password = password_hash($password, PASSWORD_DEFAULT);
-        }
+            if ($password !== $cpass) {
+                $error[] = "Les mots de passe ne correspondent pas";
+            } else {
+                $cpass = null;
+                $password = password_hash($password, PASSWORD_DEFAULT);
 
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $errors[] = 'email invalide';
-        }
-
-
-
-        if (empty($errors)) {
-
-            try {
-                $state = $pdo->prepare("SELECT COUNT(*) AS user_number FROM users WHERE username = :username");
-                $state->bindParam(':username', $username, PDO::PARAM_STR);
-                $state->execute();
-                $res = $state->fetch();
-            } catch (Exception $e) {
-                $errors[] = "Erreur de verification du username {$e->getMessage()}";
+                try {
+                    $statement = $pdo->prepare("UPDATE `users` SET password = :password  WHERE id = :id");
+                    $statement->bindParam(":id", $id, PDO::PARAM_INT);
+                    $state->bindParam(':password', $password);
+                    $statement->execute();
+                } catch (Exception $e) {
+                    $error[] = "Erreur de requête : {$e->getMessage()}";
+                }
             }
 
-            if ($res['user_number'] !== 0) {
-                $errors[] = 'Le username est déjà utilisé';
-            }
 
-            try {
-                $state = $pdo->prepare('INSERT INTO users (`username`, `email`, `password`, `enabled`)
-                    VALUES (:username, :email, :password, :enabled)');
-                $state->bindParam(':username', $username);
-                $state->bindParam(':email', $email);
-                $state->bindParam(':password', $password);
-                $state->bindParam(':enabled', $enabled, PDO::PARAM_BOOL);
-                $state->execute();
-            } catch (Exception $e) {
-                $errors[] = "Erreur à la création du user {$e->getMessage()}";
-            }
-        }
-    } else {
-        $errors[] = 'Tous les champs sont obligatoires';
+
     }
 
+    if(isset($_GET["id"]) && empty($error)) {
+        $id = $_GET["id"];
+        if(!is_numeric($id)){
+            $error[] = "ID au mauvais format";
+        } else {
+            try {
+                $statement = $pdo->prepare("SELECT `username`, `email`, `enabled` FROM `users` WHERE id = :id");
+                $statement->bindParam(":id", $id, PDO::PARAM_INT);
+                $statement->execute();
+                $user = $statement->fetch();
+            } catch (Exception $e) {
+                $error[] = "Erreur de requête : {$e->getMessage()}";
+            }
+        }
+    }
 
-}
+    if(isset($_POST['valid_button']) && empty($error)) {
+
+        $username = !empty($_POST['name'])? $_POST['name'] : null;
+        $email = !empty($_POST['mail'])? $_POST['mail'] : null;
+        $password = !empty($_POST['pass'])? $_POST['pass'] : null;
+        $cpass = !empty($_POST['cpassword'])? $_POST['cpassword'] : null;
+        $enable = !empty($_POST['enable'])? true : false;
+
+        if($username != null &&
+            $email != null &&
+            $password != null &&
+            $cpass != null)
+        {
+            $username = cleanString($username);
+            $email = cleanString($email);
+            $password = cleanString($password);
+            $cpass = cleanString($cpass);
+
+            if($password !== $cpass){
+                $error[] = "Les mots de passe ne correspondent pas";
+            } else {
+                $cpass = null;
+                $password = password_hash($password, PASSWORD_DEFAULT);
+            }
+
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                $error[] = "L'email n'est pas valide";
+            }
+
+            if(empty($error)) {
+                try {
+                    $state = $pdo->prepare("SELECT COUNT(*) AS usernb FROM `users` WHERE `email` = :mail");
+                    $state->bindParam(':mail', $email);
+                    $state->execute();
+                    $result = $state->fetch();
+                } catch (Exception $e) {
+                    $error[] = " Erreur de verifiaction de la BDD{$e->getMessage()}";
+                }
+
+                if($result['usernb'] !== 0){
+                    $error[] = "l'email est déjà utiliser";
+                }
+
+                try {
+                    $state = $pdo->prepare("INSERT INTO `users`(`username`, `password`, `email`, `enabled`)
+                    VALUES (:username, :password, :mail, :enable)");
+                    $state->bindValue(':username', $username);
+                    $state->bindValue(':password', $password);
+                    $state->bindValue(':mail', $email);
+                    $state->bindValue(':enable', $enable, PDO::PARAM_BOOL);
+                    $state->execute();
+                } catch (Exception $e) {
+                    $error[] = " Erreur a la creation du user dans la BDD{$e->getMessage()}";
+                }
+
+            }
+
+        } else {
+            $error[] = "Veuillez remplir tous les champs";
+        }
+    }
+
 ?>
-<!DOCTYPE html>
-<html lang="fr">
 
+<html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <title>Mon formulaire</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Test de formulaire</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    <div class="container">
-        <?php if (!empty($errors)): ?>
-            <?php foreach ($errors as $error): ?>
-                <div class="alert alert-danger" role="alert">
-                    <?php echo $error; ?>
-                </div>
-            <?php endforeach; ?>
-        <?php endif; ?>
-        <form method="post">
-            <div class="mb-3">
-                <label for="username" class="form-label">Identifiant</label>
-                <input type="text" name="username" id="username" class="form-control" required>
+<div class="container col-4">
+    <?php if(!empty($error)): ?>
+        <?php foreach($error as $value): ?>
+            <div class="alert alert-danger mt-2" role="alert">
+                <?php echo $value; ?>
             </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" name="email" id="email" class="form-control" required>
-            </div>
-            <div class="mb-3">
-                <label for="pass" class="form-label">Mot de passe</label>
-                <input type="password" name="pass" id="pass" class="form-control" required>
-            </div>
-            <div class="mb-3">
-                <label for="confirmation" class="form-label">Confirmation du mot de passe</label>
-                <input type="password" name="confirmation" id="confirmation" class="form-control" required>
-            </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="enabled" name="enabled">
-                <label class="form-check-label" for="enabled">Actif</label>
-            </div>
-            <div class="mb-3 d-flex justify-content-end">
-                <button type="submit" class="btn btn-primary" name="valid_button">Enregistrer</button>
-            </div>
-        </form>
-    </div>
+        <?php endforeach;?>
+    <?php endif;?>
+</div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
+<h1 class="text-center"><?php echo isset($_GET['id']) ? "Modification" : "Inscription";?></h1>
+<div class="container col-4">
+    <form method="post">
+        <div class="mb-3">
+            <label for="name "class="form-label">Username</label>
+            <input type="text" name="name" class="form-control" value="<?php echo isset($user['username']) ? $user['username'] : ''?>" required>
+        </div>
+        <div class="mb-3">
+            <label for="mail" class="form-label">Email</label>
+            <input type="email" name="mail" class="form-control" value="<?php echo isset($user['email']) ? $user['email'] : ''?>" required>
+        </div>
+        <div class="mb-3">
+            <p>Les espaces sont interdit</p>
+            <label for="pass" class="form-label">Password</label>
+            <input type="password" class="form-control" id="pass" name="pass" <?php echo isset($_GET['id']) ? null : 'required'; ?>>
+        </div>
+        <div class="mb-3">
+            <label for="cpassword" class="form-label">Confirm Password</label>
+            <input type="password" class="form-control" id="pass" name="cpassword" <?php echo isset($_GET['id']) ? null : 'required'; ?>>
+        </div>
+        <div class="mb-3 form-check">
+            <label for="enable" class="form-check-label">Enable</label>
+            <input
+                    type="checkbox"
+                    class="form-check-input"
+                    id="enable"
+                    name="enable"
+                <?php echo isset($user['enabled']) && $user['enabled'] ? 'checked' : null; ?>>
+        </div>
+        <button
+                type="submit"
+                class="btn  <?php echo isset($_GET['id']) ? 'btn-success' : 'btn-primary'; ?>"
+                name="<?php echo isset($_GET['id']) ? 'modif_button' : 'valid_button';?>">
+            <?php echo isset($_GET['id']) ? 'Editer' : 'Crée';?>
+        </button>
+    </form>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
-
 </html>
